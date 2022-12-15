23584fd0a846f57c8adbde7bdabcf0938ac87d86
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-15 01:11:49 +0100

    initialize dashboard component (leads, form

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,15 +1,18 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
+import React, { Component, Fragment } from "react";
 import Header from "./layouts/Header";
+import Dashboard from "./leads/Dashboard";
 
 class App extends Component {
     render() {
         return (
-            <Header />
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
         )
     }
 }
-
-// ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+import Form from './Form';
+import Leads from './Leads';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Form />
+        <Leads />        
+      </Fragment>
+    )
+  }
+}
+
+export default Dashboard;
